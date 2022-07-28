7265b871d5559d0c1d1df9c30c1486ebf281f485
Janne-Juhani Heino <janne.juhani.heino@gmail.com>
2022-07-28 10:00:16 +0300

    Osa_2_3_2

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const app = express()
 const cors = require('cors')
 const Person = require('./models/person')
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
+
 
 // Tehtävän 3.7 ratkaisu:
 // app.use(morgan('tiny'))
+
+
 // En onnistunut yhdistämään tokeneita esimääritellyn muotoilun kanssa, joten ratkaisin
 // tehtävän 3.8 korvaamalla tinyn sen määrittävillä valmiilla tokeneilla ja
 // listäämällä niiden perään itse määrittelemäni tokenin tulostamaan pyynnön sisällön:
-
 morgan.token('req-body', function (req, res) {
   const result = JSON.stringify(req.body)
   // Tyhjät aaltosulkeet muiden kuin POST-pyyntöjen kohdalla näyttää huonolta, joten
@@ -28,89 +30,119 @@ morgan.token('req-body', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
-//let persons = [
-    //{
-      //"id": 1,
-      //"name": "Arto Hellas",
-      //"number": "040-123456"
-      
-    //},
-    //{
-      //"id": 2,
-      //"name": "Ada Lovelace",
-      //"number": "39-44-5323523"  
-    //},
-    //{
-      //"id": 3,
-      //"name": "Dan Abramov",
-      //"number": "12-43-234345"
-    //},
-    //{
-      //"id": 4,
-      //"name": "Mary Poppendieck",
-      //"number": "39-23-6423122"    
-    //}
-//]
+
+//***************************************************************************************
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+
+// Infoa listan koosta ja lisäksi päiväys.
 app.get('/info', (req, res) => {
-  let date = new Date()
-  const text = `<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`
-  res.send(text)
+  Person.find().then(persons => {
+    let date = new Date()
+    const text = `<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`
+    res.send(text)
+  })
 })
 
+
+// Henkilön lisäys luetteloon:
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  // Vaaditaan, että lisättävällä on sekä nimi että numero:
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  // Tarkistaa, ettei luettolossa ole samannimisiä henkilöitä ennen tallentamista:
+  Person.find({ name: body.name }).limit(1).size()
+    .then(haku => {
+      if (haku.length > 0) {
+        return response.status(400).json({ 
+          error: 'name must be unique' 
+        })
+      } else {
+        
+          const person = new Person({
+            name: body.name,
+            number: body.number
+          })
+
+        person.save().then(savedPerson => {
+          response.json(savedPerson)
+        })
+      }
+  })
+})
+
+
+// Kaikkien henkilöiden tietojen palauttaminen:
 app.get('/api/persons', (req, response) => {
     Person.find().then(persons => {
     response.json(persons)
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
-})
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
-})
-
-//const generateId = () => {
-  //return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-//}
-
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+// Henkilön tietojen palauttaminenn id:n perusteella:
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
-  }
-  
-  //if (persons.find(person => person.name === body.name)) {
-    //return response.status(400).json({ 
-      //error: 'name must be unique' 
-    //})
-  //}
-
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
-
-  //persons = persons.concat(person)
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-
+    .catch(error => {
+      next(error)
+    })
 })
+
+// Henkilön tietojen ppoistaminen:
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  Person.updateOne( { _id: request.params.id }, { $set: { "number": request.body.number} } )
+    .then(result => {
+      return response.status(200).send({ id: request.params.id, name: request.body.name, number: request.body.number})
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// virheellisten pyyntöjen käsittely
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
